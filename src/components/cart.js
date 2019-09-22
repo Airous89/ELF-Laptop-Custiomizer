736500c1922480210ee14cefee0adc281c2f3628
Airous89 <50056994+Airous89@users.noreply.github.com>
2019-09-22 18:01:37 -0400

    Add files via upload

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,35 +1,18 @@
 import React, { Component } from 'react';
+import Options from './options';
+import Total from './total'
+import './cart.css';
 
 
 
-class Cart extends Component{
-  render (){
-    const Cart = Object.keys(this.props.selected)
-    .reduce((acc, curr) => acc + this.props.selected[curr].cost,0);
-    const TotalCost = Object.keys(this.props.selected)
-    .map(key =>  <div className="summary__option" key={key}>
-      <div className="summary__option__label">{key}</div>
-      <div className="summary__option__value">{this.props.selected[key].name}</div>
-      <div className="summary__option__cost">
-        {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'}).format(this.props.selected[key].cost)}
-      </div>
-    </div>)
-    return(
-      <div>
-        <section className="Your cart">
-          <h3>Your Cart</h3>
-            {TotalCost}
-          <div className="summary__total"></div>
-          <div className="summary__total">Total Price: </div>
-          <div className="summary__total__value">
-            {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'}).format(Cart)}
-
-          </div>
-        </section>
-      </div>
+export default class Cart extends Component {
+  render() {
+    return (
+      <section className="main__summary">
+        <h3>Your Cart</h3>
+          < Options selected={this.props.selected} />
+          < Total selected={this.props.selected} />
+      </section>
     )
   }
 }
-
-
-export default Cart;
